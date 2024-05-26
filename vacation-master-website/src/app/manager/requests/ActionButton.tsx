232f033ccd8d0444e389fb7464db488d998dd4b4
ido vitman zilber <90967400+idogit123@users.noreply.github.com>
2024-05-26 12:31:09 +0300

    Loading animation for approve / reject buttons

--- vacation-master-website/src/app/manager/requests/ActionButton.tsx
+++ vacation-master-website/src/app/manager/requests/ActionButton.tsx
@@ -1,15 +1,24 @@
 "use client";
-import { useRouter } from "next/router";
+import { useTransition } from "react";
 import setRequestStatus from "./setRequestStatus";
+import styles from '@/app/styles/button.module.css'
 
 export default function ActionButton(
     {action, requestId}: {action: string, requestId: string}
 ) {
+    const [isPending, startTransition] = useTransition()
 
-    return <button data-action={action} onClick={async () => {
-        console.log('action', action)
-        await setRequestStatus(action, requestId)
-    }}>
-        {action}
+    return <button data-action={action} onClick={
+        () => {
+            startTransition(
+                () => setRequestStatus(action, requestId)
+            )
+        }
+    }>
+        {
+            isPending ? <div id={styles.loaderContainer} >
+                <div id={styles.loader} ></div>
+            </div> : action
+        }
     </button>
 }
